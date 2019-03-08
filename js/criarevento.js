225147d8fae05e7af23b5d03b558f920ff54db18
Ruan-Miguel <ruanmiguel14@hotmail.com>
2019-03-08 13:58:58 -0300

    funcionalidade da tela criar evento incompleta

---
+++ js/criarevento.js
@@ -0,0 +1,22 @@
+var gambiarra = 0
+
+function opcaoDoPreco () {
+	var radio = document.getElementsByName('custo')
+	var newinput = '<div id="novoinput" class="input-field col m10 offset-m1"><input id="preco" name="preco" type="text" class="validate" oninput=""><label for="preco">Preço</label></div>'
+	var formulario = document.getElementsByTagName('form')[0]
+	if (radio[0].checked == true) {
+		if(gambiarra == 1){
+			document.getElementsByTagName('form')[0].removeChild(document.getElementById('novoinput'))
+			gambiarra = 0
+		}
+	}else {
+		if (gambiarra == 0) {
+			formulario.innerHTML += newinput
+			gambiarra = 1
+		}
+	}
+}
+
+function segundoClick (elemento) {
+	elemento.click()
+}
