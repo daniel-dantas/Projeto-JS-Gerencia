dd320260a4d3424a9ff5439164e977483dbd9146
XSrDanDanX <ddipaumirim@gmail.com>
2019-03-14 09:36:05 -0300

    Backup de arquivos do banco de dados

--- eventdivulgation.sql
+++ eventdivulgation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 09-Mar-2019 às 17:50
+-- Generation Time: 14-Mar-2019 às 13:35
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 5.6.40
 
@@ -21,6 +21,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `eventdivulgation`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `evento`
+--
+
+CREATE TABLE `evento` (
+  `usuario` varchar(20) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `descricao` text NOT NULL,
+  `file` text NOT NULL,
+  `opcao` varchar(30) NOT NULL,
+  `preco` double(6,2) NOT NULL,
+  `cidade` varchar(30) NOT NULL,
+  `endereco` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -43,6 +60,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`nome`, `sobrenome`, `cidade`, `estado`, `email`, `username`, `senha`) VALUES
+('Daniel', 'Dantas', 'Ipaumirim', 'Ceará', 'daniel.dantas.ip18@gmail.com', 'dandan', 'deiniel123'),
 ('Daniel', 'Dantas', 'Ipaumirim', 'Ceará', 'daniel.dantas.ip18@gmail.com', 'xsrdandanx', '654123');
 
 --
@@ -50,10 +68,26 @@ INSERT INTO `usuario` (`nome`, `sobrenome`, `cidade`, `estado`, `email`, `userna
 --
 
 --
+-- Indexes for table `evento`
+--
+ALTER TABLE `evento`
+  ADD KEY `fk_usuario` (`usuario`);
+
+--
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `evento`
+--
+ALTER TABLE `evento`
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
