641a3b8fe5dff565a2fb61856d711e7f4df097e3
Ruan-Miguel <ruanmiguel14@hotmail.com>
2019-03-16 21:08:37 -0300

    problemas na pag criar eventos reparados

--- js/criarevento.js
+++ js/criarevento.js
@@ -2,8 +2,6 @@ var gambiarra = 0
 
 function opcaoDoPreco() {
     var radio = document.getElementsByName('custo')
-    var newinput = '<div id="novoinput" class="input-field col m10 offset-m1"><input id="preco" name="preco" type="text" class="validate" oninput=""><label for="preco">Preço</label></div>'
-    var formulario = document.getElementsByTagName('form')[0]
     if (radio[0].checked == true) {
         if (gambiarra == 1) {
             document.getElementsByTagName('form')[0].removeChild(document.getElementById('novoinput'))
@@ -11,8 +9,11 @@ function opcaoDoPreco() {
         }
     } else {
         if (gambiarra == 0) {
-            formulario.innerHTML += newinput
-            radio[1].checked = true
+            var newinput = document.createElement('div')
+            newinput.id = 'novoinput'
+            newinput.className = 'input-field col m10 offset-m1'
+            newinput.innerHTML = '<input id="preco" name="preco" type="text" class="validate" oninput=""><label for="preco">Preço</label>'
+            document.getElementsByTagName('form')[0].appendChild(newinput)
             gambiarra = 1
         }
     }
